caddcf310afdb047f303ec5742c77055a21a56d6
jun <chchdelm3@icloud.com>
2021-04-13 12:52:38 +0900

    add limit

--- Tutorial_c/guessing_game.c
+++ Tutorial_c/guessing_game.c
@@ -4,12 +4,27 @@
 int main(void){
     int secretNumber = 5;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
 
-    while(guess != secretNumber){
-        printf("Enter a number: ");
-        scanf("%d", &guess);
+    printf("Start a guessing number game!\n");
+
+    while(guess != secretNumber && outOfGuesses == 0){
+        if(guessCount < guessLimit){
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            guessCount++;
+        }
+        else{
+            outOfGuesses = 1;
+        }
     }
 
-    printf("You Win!\n");
+    if(outOfGuesses == 1)
+        printf("Out of guesses");
+    else
+        printf("You Win!");
+
     return 0;
 }
