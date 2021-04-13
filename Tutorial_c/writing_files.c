927e94b84741404b0df6d79a17dba1ceae7c2921
jun <chchdelm3@icloud.com>
2021-04-13 17:54:33 +0900

    create reading files

--- Tutorial_c/writing_files.c
+++ Tutorial_c/writing_files.c
@@ -1,21 +1,16 @@
 /* 외부 파일 읽고 쓰는 법 */
 
-/** #include <stdio.h>                                                            */
-/**                                                                               */
-/** int main(void){                                                               */
-/**     FILE *fpointer = fopen("employees.txt", "w");                             */
-/**     fprintf(fpointer, "Jim, Salesman\nPam, Receptionist\nOscar, Accounting"); */
-/**     [> fprintf(fpointer, "overwritten"); <]                                   */
-/**                                                                               */
-/**     fclose(fpointer);                                                         */
-/** }                                                                             */
-
 #include <stdio.h>
 
 int main(void){
+
+    // file mode: w 는 쓰기만 가능하여 내용을 덮어씌움.
     FILE *fpointer = fopen("employees.txt", "w");
     fprintf(fpointer, "Jim, Salesman\nPam, Receptionist\nOscar, Accounting");
-    /** fprintf(fpointer, "overwritten"); */
-
     fclose(fpointer);
+
+    // file mode: a 는 추가가 가능하여 내용을 추가함.
+    FILE *femployees = fopen("employees.txt", "a");
+    fprintf(femployees, "\nJun, Developer");
+    fclose(femployees);
 }
