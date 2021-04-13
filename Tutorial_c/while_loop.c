a9164ea27d7b18206cecc6951e8921913104a0b2
jun <chchdelm3@icloud.com>
2021-04-13 12:32:54 +0900

    create while loop

---
+++ Tutorial_c/while_loop.c
@@ -0,0 +1,13 @@
+/* while 문으로 반복 구문을 만들기 */
+
+#include <stdio.h>
+
+int main(void){
+    int index = 1;
+    while(index <= 5){
+        printf("%d\n", index);
+        index++;
+    }
+
+    return 0;
+}
